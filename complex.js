23100ec92aa35daab4680dbed0f5b45d0de63754
worldmaker <worldmaker18349276@gmail.com>
2020-04-09 22:13:24 +0800

    refine color function

--- complex.js
+++ complex.js
@@ -14,31 +14,26 @@ function modAround(phi, phi0) {
   return phi0 + dphi;
 }
 function hue2rgb(p, q, t) {
-  if ( t < 0 ) t += 1;
-  if ( t > 1 ) t -= 1;
+  t = (t % 1 + 1) % 1;
   if ( t < 1/6 ) return p + (q - p) * 6 * t;
   if ( t < 1/2 ) return q;
-  if ( t < 2/3 ) return p + (q - p) * (2/3 - t) * 6;
-  return p;
+  if ( t < 2/3 ) return q + (p - q) * 6 * (t - 1/2);
+                 return p;
 }
 function hsl2rgb(h, s, l) {
-  var r, g, b;
+  var a = l < 0.5 ? l * s : (1 - l) * s;
+  var p = l - a;
+  var q = l + a;
 
-  if ( s == 0 ) {
-    r = g = b = l; // achromatic
-  } else {
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1/3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1/3);
-  }
+  var r = hue2rgb(p, q, h + 1/3);
+  var g = hue2rgb(p, q, h);
+  var b = hue2rgb(p, q, h - 1/3);
 
   return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
-function toColor(val, l=0.5) {
+function toColor(val) {
   var [r, phi] = toRPhi(...val);
-  return hsl2rgb((phi/TAU+1)%1, Math.tanh(r), l);
+  return hsl2rgb(phi/TAU, Math.tanh(r), 0.5);
 }
 
 class Expr
